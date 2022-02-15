06eee765be5a2f129105a79c36aa03be53f93d8d
smfc0026 <kamiks45@wp.pl>
2022-02-15 20:33:06 +0100

    First Commit

---
+++ zad2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	
+	int a,b;
+	
+	cout<<"Podaj dwie liczby"<<endl;
+	cin>>a>>b;
+	
+	if (a>=b) {
+		cout<<a;
+		
+	}
+	else if (b>=a) {
+		cout<<b;
+		
+	}
+	
+	
+	return 0;
+	
+}
